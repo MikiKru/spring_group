a6248f749274cc3705cf5bf1178d15ad5db9a4fd
MikiKru <michal_kruczkowski@o2.pl>
2019-08-03 12:04:34 +0200

    s3 v7

--- src/main/java/pl/taskmanager/controller/frontEndControllers/UserControllerFrontEnd.java
+++ src/main/java/pl/taskmanager/controller/frontEndControllers/UserControllerFrontEnd.java
@@ -3,9 +3,14 @@ package pl.taskmanager.controller.frontEndControllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import pl.taskmanager.model.dto.UserDto;
 import pl.taskmanager.service.UserService;
+
+import javax.validation.Valid;
 
 @Controller
 public class UserControllerFrontEnd {
@@ -18,6 +23,21 @@ public class UserControllerFrontEnd {
     @GetMapping("/register")
     public String register(Model model){
         model.addAttribute("userDto",new UserDto());
+        model.addAttribute("password_repeat", "");
         return "register";
     }
+    @PostMapping("/register")
+    public String register(
+            @ModelAttribute @Valid UserDto userDto,
+            BindingResult bindingResult
+    ){
+        // błędy formularza
+        if (bindingResult.hasErrors()){
+            return "register";
+        }
+        // rejestracja
+        return "redirect:/projects";
+    }
+
+
 }
