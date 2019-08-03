b64294fc11dc45e98d4dd44b991cb57886edfd93
MikiKru <michal_kruczkowski@o2.pl>
2019-08-03 14:01:32 +0200

    s3 v12

--- src/main/java/pl/taskmanager/configuration/WebSecurityConfiguration.java
+++ src/main/java/pl/taskmanager/configuration/WebSecurityConfiguration.java
@@ -15,15 +15,23 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .antMatchers("/addProject").hasAnyAuthority("ROLE_ADMIN")
                 .antMatchers("/projects&delete**").hasAnyAuthority("ROLE_ADMIN")
                 .antMatchers("/addTask**").hasAnyAuthority("ROLE_ADMIN")
-                .antMatchers("/addTask**").hasAnyAuthority("ROLE_ADMIN")
                 .antMatchers("/task&delete**").hasAnyAuthority("ROLE_ADMIN")
                 // pozostałe nie wymagają logowania
                 .anyRequest().permitAll()
                 // wstrzyknięcie podstawowego formularza logowania
                 .and()
                     .formLogin()
-                        .and()
-                    .httpBasic();
+                        // adres formularza logowania
+                        .loginPage("/login")
+                        // nazwy zmiennych z formularza (th:name)
+                        .usernameParameter("email")
+                        .passwordParameter("password")
+                        // adres przekazania wartości po wysłaniu formularza
+                        // th:action
+                        .loginProcessingUrl("/login-process")
+                        // przekierowanie po zalogowaniu
+                        .defaultSuccessUrl("/projects");
+
 
     }
 
