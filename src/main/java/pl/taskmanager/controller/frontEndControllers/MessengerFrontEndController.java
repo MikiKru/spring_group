c92ee74fade60c8f9eaa2322260b5dcbdb65e2ef
MikiKru <michal_kruczkowski@o2.pl>
2019-08-10 14:07:07 +0200

    spring d5 v7

---
+++ src/main/java/pl/taskmanager/controller/frontEndControllers/MessengerFrontEndController.java
@@ -0,0 +1,54 @@
+package pl.taskmanager.controller.frontEndControllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import pl.taskmanager.model.dto.CommentDto;
+import pl.taskmanager.model.dto.ContactDto;
+import pl.taskmanager.repository.UserRepository;
+
+import javax.validation.Valid;
+
+@Controller
+public class MessengerFrontEndController {
+
+    private UserRepository userRepository;
+    @Autowired
+    public MessengerFrontEndController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping("/sendMessage")
+    public String sendMessage(
+            Model model
+    ){
+        // lista wszystkich użytkowników
+        model.addAttribute("users", userRepository.findAll());
+        // przekazanie do widoku modelu contactDto
+        model.addAttribute("contactDto", new ContactDto());
+        return "contact";
+    }
+
+    @PostMapping("/sendMessage")
+    public String sendMessage(
+            @ModelAttribute @Valid ContactDto contactDto,
+            BindingResult bindingResult,
+            Authentication auth
+    ){
+//        if(bindingResult.hasErrors()){
+//            return "contact";
+//        }
+        // auto mailing
+        System.out.println("Content: " + contactDto.getContent());
+        System.out.println("User: " + contactDto.getEmailTo());
+        return "redirect:/sendMessage";
+    }
+
+
+
+}
